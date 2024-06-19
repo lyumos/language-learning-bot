2120f5cffc1aa91d8e6cc2a2f45b33f707a59c97
lyumos <lyumosvl@yandex.ru>
2024-06-19 17:34:20 +0300

    Initial commit

---
+++ db/db_creation.sql
@@ -0,0 +1,39 @@
+BEGIN;
+
+CREATE TABLE "examples" (
+	"id"	TEXT NOT NULL UNIQUE,
+	"word_id"	TEXT NOT NULL,
+	"example"	TEXT NOT NULL,
+	"translation"	TEXT NOT NULL,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("word_id") REFERENCES "words"("id")
+);
+
+CREATE TABLE "definitions" (
+	"id"	TEXT NOT NULL UNIQUE,
+	"word_id"	TEXT NOT NULL,
+	"definitions"	TEXT NOT NULL,
+	"language"	TEXT NOT NULL,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("word_id") REFERENCES "words"("id")
+);
+
+CREATE TABLE "tests" (
+	"id"	TEXT NOT NULL UNIQUE,
+	"word_id"	TEXT NOT NULL,
+	"date"	TEXT NOT NULL,
+	"status"	TEXT NOT NULL,
+	FOREIGN KEY("word_id") REFERENCES "words"("id")
+);
+
+CREATE TABLE "words" (
+	"id"	TEXT NOT NULL UNIQUE,
+	"word"	TEXT NOT NULL,
+	"translation" TEXT,
+	"synonyms" TEXT,
+	"antonyms" TEXT,
+	"status"	TEXT NOT NULL,
+	PRIMARY KEY("id")
+);
+
+COMMIT;
